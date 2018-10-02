751efa998fbc3a1c6426e84b80bcea417be37db2
Gert-Jaap Glasbergen <gertjaap@gertjaap.nl>
2018-10-02 12:09:20 +0200

    Added gomobile building for iOS

--- lit-rpc-proxy/build.sh
+++ lit-rpc-proxy/build.sh
@@ -4,5 +4,6 @@ mkdir -p ../android/litrpcproxy
 rm ../android/litrpcproxy/litrpcproxy.aar
 mv litrpcproxy.aar ../android/litrpcproxy
 
+gomobile bind -target=ios
+cp -r Litrpcproxy.framework ../ios/LitPOC
 
-## TODO: IOS
